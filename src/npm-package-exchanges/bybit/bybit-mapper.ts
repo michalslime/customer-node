b7844a213349a07377bde6a294f9bac46a2d2a51
Michał Strzoda <michal.strzoda@gmail.com>
2025-12-12 16:14:12 +0100

    Adds position details and profit calculation
    Adds average price, profit percentage, updated timestamp and leverage to the position model.
    
    Calculates profit/loss as a percentage of the average price, taking into account the position side (buy/sell) and leverage.
    
    Includes a check to avoid division by zero when calculating percentage change.

--- src/npm-package-exchanges/bybit/bybit-mapper.ts
+++ src/npm-package-exchanges/bybit/bybit-mapper.ts
@@ -12,8 +12,12 @@ export function BybitPositionMapper(positionResponse: PositionV5) {
         position.takeProfit = positionResponse.takeProfit === '' || positionResponse.takeProfit === undefined || positionResponse.takeProfit === null ? undefined : parseFloat(positionResponse.takeProfit);
         position.stopLoss = positionResponse.stopLoss === '' || positionResponse.stopLoss === undefined || positionResponse.takeProfit === null ? undefined : parseFloat(positionResponse.stopLoss);
         position.price = parseFloat(positionResponse.markPrice);
+        position.avgPrice = parseFloat(positionResponse.avgPrice);
         position.pnl = parseFloat(positionResponse.unrealisedPnl);
         position.size = parseFloat(positionResponse.size);
+        position.profitPercent = percentChange(position.avgPrice, position.price, position.side) * parseFloat(positionResponse.leverage || '1');
+        position.updated = parseInt(positionResponse.updatedTime);
+        position.leverage = parseFloat(positionResponse.leverage || '1');
         position.originalPosition = positionResponse;
     } catch (err) {
         console.error(err);
@@ -27,4 +31,14 @@ function mapBybitSide(side: PositionSideV5): Side {
         return side;
     }
     throw new Error(`Invalid side value received: ${side}`);
+}
+
+function percentChange(openPrice: number, currentPrice: number, side: Side): number {
+    if (openPrice === 0) {
+        throw new Error("openPrice cannot be zero.");
+    }
+
+    const result = ((currentPrice - openPrice) / openPrice) * 100;
+
+    return side === "Sell" ? -result : result;
 }
